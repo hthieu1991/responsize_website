a519931fb308f4feb10f79b14fa311986e556a61
ruby -v <programming.iter@gmail.com>
2014-11-09 23:37:20 +0700

    add chitietbaiviet

---
+++ chi-tiet-bai-viet.php
@@ -0,0 +1,20 @@
+<?php include 'inc/top.php';?>
+    <div class="row">
+		
+        <div class="col-sm-9">
+        	<h3 align="center" class="jumbotron h-about">Bài 1 : Cài đặt môi trường lập trình</h3>
+			<div class="bs-example">
+				<p>Nội dung bài viết được đặt ở đây</p>
+				<div align="center" class="row">   
+					<div class="fb-comments" data-href="https://www.facebook.com/laptrinhkungfu" data-numposts="10" data-colorscheme="light" width="100%"></div>
+				</div>
+			</div>
+        </div>
+
+        <!-- sidebar -->
+		<?php include 'inc/sidebar.php';?>
+        <!-- end sidebar -->
+
+      </div>
+   </div>
+  <?php include 'inc/footer.php';?>
