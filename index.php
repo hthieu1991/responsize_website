632de5ef4bf4a95ef4161446f924ae3f561d1f79
ruby -v <programming.iter@gmail.com>
2014-11-09 09:25:33 +0700

    edited index.php

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
-
+    <title>Lập trình PHP - PHP Programing - PHP Development</title>
+    <meta content="Blog chia sẻ kinh nghiệm hướng dẫn lập trình PHP. Framework PHP" name="description">
+	<meta content="php, lap trinh php, php tutorials, php programming" name="keywords">
+	<link href="http://kungfuphp.com" rel="canonical">
+	<meta content="log chia sẻ kinh nghiệm hướng dẫn lập trình PHP. Framework PHP" property="og:description">
+	<meta content="http://kungfuphp.com" property="og:url">
+	<meta content="https://www.facebook.com/laptrinhkungfu" property="article:publisher">
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
@@ -20,13 +25,23 @@
   	<!-- Nav Bar -->
   <div class="container">	
   	<div class="header">
-        <ul role="tablist" class="nav nav-pills pull-right">
+        <ul role="tablist" class="nav nav-pills pull-right col-sm-3 h-nav-margin">
           <li class="active" role="presentation"><a href="#">Home</a></li>
           <li role="presentation"><a href="#">Blogs</a></li>
           <li role="presentation"><a href="#">Contact</a></li>
         </ul>
-        <h3 class="text-muted">Trong Hieu's Blog</h3>
+        <h3 class="text-muted col-sm-3">Trong Hieu's Blog</h3>
+        <form role="search" class="navbar-form col-sm-6 h-search-padding">
+	            <div class="form-group">
+	              <input type="text" placeholder="Search" class="form-control">
+	            </div>
+	            <button class="btn btn-default" type="submit">Submit</button>
+	    </form>
+        
       </div>
+    <div class="row">
+	    
+    </div>
 	<div class="row">
 		<div class="col-sm-9">
 			<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
@@ -111,31 +126,31 @@
             <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
           </div><!-- /.blog-post -->
 
-          <div class="blog-post">
-            <h2 class="blog-post-title">Another blog post</h2>
-            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+<!--          <div class="blog-post">-->
+<!--            <h2 class="blog-post-title">Another blog post</h2>-->
+<!--            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>-->
+<!---->
+<!--            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>-->
+<!--            <blockquote>-->
+<!--              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>-->
+<!--            </blockquote>-->
+<!--            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>-->
+<!--            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>-->
+<!--          </div>-->
 
-            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-            <blockquote>
-              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            </blockquote>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          </div><!-- /.blog-post -->
-
-          <div class="blog-post">
-            <h2 class="blog-post-title">New feature</h2>
-            <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
-
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <ul>
-              <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-              <li>Donec id elit non mi porta gravida at eget metus.</li>
-              <li>Nulla vitae elit libero, a pharetra augue.</li>
-            </ul>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-          </div><!-- /.blog-post -->
+<!--          <div class="blog-post">-->
+<!--            <h2 class="blog-post-title">New feature</h2>-->
+<!--            <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>-->
+<!---->
+<!--            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>-->
+<!--            <ul>-->
+<!--              <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>-->
+<!--              <li>Donec id elit non mi porta gravida at eget metus.</li>-->
+<!--              <li>Nulla vitae elit libero, a pharetra augue.</li>-->
+<!--            </ul>-->
+<!--            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>-->
+<!--            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>-->
+<!--          </div>-->
 
           <nav>
             <ul class="pager">
@@ -144,7 +159,7 @@
             </ul>
           </nav>
 
-        </div><!-- /.blog-main -->
+        </div>
 
         <div class="col-sm-3 h-margin-top">
           <div class="jumbotron h-about">
@@ -154,7 +169,13 @@
             </p>
           </div>
           <div class="sidebar-module">
-          	Fabook Like and share
+          	<ul class="nav nav-pills nav-stacked" role="tablist">
+			  <li role="presentation" class="active"><a href="#">PHP cơ bản</a></li>
+			  <li role="presentation"><a href="#">PHP nâng cao</a></li>
+			  <li role="presentation"><a href="#">Laravel Framework</a></li>
+			  <li role="presentation"><a href="#">AJAX</a></li>
+			  <li role="presentation"><a href="#">Plugin js</a></li>
+			</ul>
           </div>
           <div class="sidebar-module">
             <h4>Archives</h4>
@@ -184,8 +205,19 @@
         </div><!-- /.blog-sidebar -->
 
       </div>
-    
    </div>
+   <div class="jumbotron h-about">
+   			<p style="padding-left:50px;font-size:13px;">Copyright 2014-2015 © kungfuphp.com | Powered By KungfuPhp Version 1.0</p>
+   			<div align="center">
+		    	<a title="Hoc PHP Mysql Jquery" href="http://www.kungfuphp.com"> PHP MYSQL Online</a> - 
+				<a title="Hoc PHP" href="http://www.qhonline.info"> Hoc PHP</a> - 
+				<a title="PHP Co ban" href="http://www.kungfuphp.com"> PHP co ban</a> - 
+				<a title="PHP Nang Cao" href="http://www.kungfuphp.com"> Lap Trinh PHP Nang Cao</a> - 
+				<a title="Laravel Framework" href="http://www.kungfuphp.com"> PHP Framework</a> - 
+				<a target="_blank" title="Fabook kungfuphp" href="http://facebook.com/laptrinhkungfu">Facebook KungfuPhp</a>
+			</div>
+   </div>  
+   
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -231,6 +263,12 @@
 	}
 	.has-border{
 		border : solid 3px;
+	}
+	.h-nav-margin{
+		margin-top:10px !important;
+	}
+	.h-search-padding{
+		padding-top:7px;
 	}
 
   </style>
