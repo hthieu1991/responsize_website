f2f90697548450081d54a52af03ac66654edb518
ruby -v <programming.iter@gmail.com>
2014-11-11 22:34:50 +0700

    test

--- index.php
+++ index.php
@@ -4,16 +4,18 @@
         <div class="col-sm-9">
 
           <div class="blog-post">
-            <h2 class="blog-post-title">Thế giới lập trình web</h2>
+            <h2 class="blog-post-title">Lập trình web là gì ?</h2>
             <p class="blog-post-meta">11/11/2012 by <a href="#">Administrator</a></p>
 
-            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+            <p>Bài viết này tóm tắt ngắn gọn, tổng quan về lập trình web là gì ?</p>
             <hr>
-            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+            <p>Để tìm hiểu <a href="#">lập trình web là gì</a>, thì chúng ta nên hiểu rõ khái niệm "lập trình" là gì ?</p>
             <blockquote>
-              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p>Lập trình đó là <strong>công đoạn nghệ thuật phức tạp yêu cầu máy tính cần phải làm những gì</strong></p>
             </blockquote>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>Bạn đừng hiểu lầm lập trình theo nghĩa <em>"ê, lấy cho tao ly nước uống coi, cái thằng máy tính ngu ngốc kia"</em>. Nó không phải vậy, mà lập trình được hướng dẫn thực thi bằng một chuỗi các mã lệnh, bố cục hợp lý để máy tính có thể hiểu được
+              . Về cơ bản, bạn cung cấp cho máy tính một văn bản bao gồm các bước nhỏ hướng dẫn, máy tính sẽ đi theo từng bước nhỏ đó đến cuối danh sách
+            </p>
             <h2>Heading</h2>
             <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
             <h3>Sub-heading</h3>
