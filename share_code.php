36136a4b38fd4e7cf14d23c9041e5653fff36112
hthieu1991 <programming.iter@gmail.com>
2014-12-04 13:00:35 +0700

    t

---
+++ share_code.php
@@ -0,0 +1,15 @@
+<!-- Kungfuphp BEGIN -->
+<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+<a class="a2a_dd" href="https://www.addtoany.com/share_save?linkurl=http%3A%2F%2Fkungfuphp.com&amp;linkname=Kungfu%20PHP"></a>
+<a class="a2a_button_facebook"></a>
+<a class="a2a_button_twitter"></a>
+<a class="a2a_button_google_plus"></a>
+</div>
+<script type="text/javascript">
+var a2a_config = a2a_config || {};
+a2a_config.linkname = "Kungfu PHP";
+a2a_config.linkurl = "http://kungfuphp.com";
+a2a_config.locale = "vi";
+</script>
+<script type="text/javascript" src="//static.addtoany.com/menu/page.js"></script>
+<!-- Kungfuphp END -->
